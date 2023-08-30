00c328dce3b3336f46bf431c44c3e7b5dd30ee39
rushifuse <128386609+rushifuse@users.noreply.github.com>
2023-08-30 19:37:55 +0530

    Add files via upload

---
+++ ABC.java
@@ -0,0 +1,30 @@
+class ABC
+{
+public static void main(String args[])
+{char ch='A';
+	
+	for(int i=0;i<5;i++)
+	{
+	
+		for(int j=0;j<=i;j++)
+			
+		{
+			System.out.print(ch+" ");
+		}
+		ch++;
+		System.out.println();
+	}
+	
+
+}}
+
+
+//output
+/*
+A
+B B
+C C C
+D D D D
+E E E E E
+
+*/
