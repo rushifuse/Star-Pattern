1948b3cede5dbc4aef61d0f95eaaa157562a3bd0
rushifuse <128386609+rushifuse@users.noreply.github.com>
2023-08-30 19:25:33 +0530

    Add files via upload

---
+++ SUM.java
@@ -0,0 +1,19 @@
+class SUM
+{
+public static void main(String args[])
+{
+	
+	for(int i=0;i<5;i++)
+	{
+		int k=1;
+		for(int j=0;j<=i;j++)
+		{
+		
+			System.out.print(k++ +" ");
+		}
+		System.out.println();
+	}
+	
+	
+	
+}}
