bf336d69b5f3e9f01d9a19afa7afb97d248c3b2b
rushifuse <128386609+rushifuse@users.noreply.github.com>
2023-08-31 19:32:29 +0530

    Add files via upload

---
+++ Star6.java
@@ -0,0 +1,50 @@
+class Star6{
+	public static void main(String args[]){
+		
+		for(int i=1;i<=5;i++)
+		{
+			
+			for(int j=5-i;j>=1;j--)
+			{
+				System.out.print(" ");
+			}
+			
+			for(int j=1;j<=i;j++)
+			{
+				System.out.print("* ");
+			}
+			System.out.println();
+			
+		}
+		System.out.print(" ");
+		
+		for(int i=4;i>=1;i--)
+		{
+			
+			for(int j=4-i;j>=1;j--)
+			{
+				System.out.print(" ");
+			}
+			
+			for(int j=1;j<=i;j++)
+			{
+				System.out.print("* ");
+			}
+			System.out.println();
+			
+			System.out.print(" ");
+			
+		}
+		
+	}
+}
+
+/*
+output:-
+* * * * *
+  * * * *
+    * * *
+      * *
+        *
+
+*/
